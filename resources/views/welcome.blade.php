a98375dd12aa561416a218407b7931368de28602
Rory Laster <rorclast@gmail.com>
2019-01-10 17:08:40 -0800

    Changing welcome-view font to Indie Flower.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
         <title>Rory C. Laster's Site</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Indie Flower', cursive;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
